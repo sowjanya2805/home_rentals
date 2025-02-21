b2f1b7c5123be966839ced9a34b792192015cf51
sowjanya2805 <sowjanyajallu2004@gmail.com>
2025-02-22 04:34:33 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -4,9 +4,20 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
+const authRoutes = require("./routes/auth.js")
+const listingRoutes = require("./routes/listing.js")
+const bookingRoutes = require("./routes/booking.js")
+const userRoutes = require("./routes/user.js")
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+/* ROUTES */
+app.use("/auth", authRoutes)
+app.use("/properties", listingRoutes)
+app.use("/bookings", bookingRoutes)
+app.use("/users", userRoutes)
 
 /* MONGOOSE SETUP */
 const PORT = 3001;
